f02d9158edf14cfc3684b2461bb86acea2cd45b2
Mohammad Jihad Uddin <mjihaduddin00@outlook.com>
2021-03-03 17:29:41 -0600

    Main File
    I am committing the main file that the application is comprised of (Java File).

---
+++ Solution.java
@@ -0,0 +1,14 @@
+/* 
+Printing strings
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        writeToConsole("Hello, World!");
+    }
+
+    public static void writeToConsole(String s) {
+        System.out.println("printing: " + s);
+    }
+}
